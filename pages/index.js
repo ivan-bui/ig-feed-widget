631b474dcd06215a137c6983f626d773291df99c
ivan-bui <ibui4190@mail.com>
2026-01-01 11:51:40 -0500

    Add index.js for vercel build

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import InstagramFeed from '../components/InstagramFeed';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <InstagramFeed />
+    </div>
+  );
+}
