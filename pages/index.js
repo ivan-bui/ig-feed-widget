8838b245af6400a1c4b20aa7396d2ac65f0c8b2e
ivan-bui <ibui4190@mail.com>
2026-01-03 00:49:21 -0500

    remove router

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import InstagramFeed from '../components/InstagramFeed';
-
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <InstagramFeed />
-    </div>
-  );
-}
